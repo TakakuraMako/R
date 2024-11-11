57a332adee60c72b859ab91866d3b4523b30b4e2
TakakuraMako <1361942776@qq.com>
2024-11-11 15:01:41 +0800

    upgrade

--- test/1.R
+++ test/1.R
@@ -1,2 +1,12 @@
+# 赋值
 mystring <- "Hello World"
-print(mystring)
+# print(mystring)
+
+# 向量组合、命名
+a <- c(1, 2, 3)
+names(a) <- c("aa", "ab", "ac")
+print(a)
+
+# 列表
+b <- list(1, 2, 3)
+print(b[[1]] + b[[3]])
