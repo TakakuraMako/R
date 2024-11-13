285d0d71ac3b99fdb4683fe36e2cac54748acb78
TakakuraMako <1361942776@qq.com>
2024-11-13 14:50:10 +0800

    upgrade

--- test/1.R
+++ test/1.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 # 赋值
 mystring <- "Hello World"
 # print(mystring)
@@ -8,10 +9,33 @@ names(a) <- c("aa", "ab", "ac")
 print(a)
 
 # 列表
-b <- list(1, 2, 3)
-print(b[[1]] + b[[3]])
+b <- list(
+  a = c(5, 10),
+  b = c("I", "love", "R", "language", "!"),
+  c = c(TRUE, TRUE, FALSE, TRUE)
+)
 
+print(b[[1]])
+
+# 数据框
+df <- data.frame(
+  name      = c("Alice", "Bob", "Carl", "Dave"),
+  age       = c(23, 34, 23, 25),
+  marriage  = c(TRUE, FALSE, TRUE, FALSE),
+  color     = c("red", "blue", "orange", "purple")
+)
+print(df["name"], df[[1]])
+# 函数
 sum <- function(x){
   x + 1
 }
 sum(a)
+
+# 矩阵
+m <- matrix(
+  c(2, 4, 3, 1, 5, 7),
+  nrow = 2, 
+  ncol = 3,
+  byrow = TRUE
+)
+print(m)
