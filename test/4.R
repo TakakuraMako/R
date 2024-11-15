a39a7f27791e161f07d5badfea45497820a36b36
TakakuraMako <1361942776@qq.com>
2024-11-15 16:09:49 +0800

    upgrade

--- test/4.R
+++ test/4.R
@@ -1,23 +1,25 @@
 library(dplyr)
-data <- read.table(here::here("data", "545110-99999-2023"), header = TRUE)
+data <- read.table(here::here("data", "545110-99999-2023"), header = FALSE)
+data <- data[, 1:6]
+colnames(data) <- c("year", "month", "day", "hour", "higest", "lowest")
+print(data)
+data[,5:6] <- data[, 5:6]/10
+print(data)
+# data %>% is.na()
+# new_col <- c(1, 2)
+# # 新增列
+# new <- mutate(data[1:2, ], new_col, col_name = c(1))
+# print(new)
 
-new_col <- c(1, 2)
-new <- mutate(data[1:2, ], new_col, col_name = c(1))
-print(new)
-
-c(1:10) %>% sum()
-# 等价
-sum(c(1:10))
-c(-10:10) %>% abs() %>% sum() %>% sqrt() #从左到右
-
-print(select(data, 1))
-data %>% select(ends_with("r"))
-data %>% select(where(is.numeric) & starts_with("2"))
-data %>% rename(year = X2023)
-data %>% filter(X01.1 == 4)
-data %>% group_by(X01) %>% summarise(mean_X.59 = mean(X.59))
-data %>% arrange(X.59)
-
+# 选择列
+data[1:5, ] %>% select(ends_with("r"))
+data[1:5, ] %>% select(where(is.numeric) & starts_with("d"))
+data %>% rename(days = day)
+data %>% filter(hour == 4)
+data %>% group_by(hour) %>% summarise(mean_higest = mean(higest))
+data %>% arrange(lowest)
+data <- data %>% filter(lowest > -19.8) %>% drop()
+print(data)
 # df1 <- df_new %>% 
 #   group_by(name) %>% 
 #   summarise( 
